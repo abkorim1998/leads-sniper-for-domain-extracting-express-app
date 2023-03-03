95b4d2cd1185f5a2a2fb83cd44f6da04bb07fa9e
abkorim1998 <abkorim1998@gmail.com>
2023-03-03 23:36:56 +0600

    new upload

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', async (req, res) => {
     res.send('Hello World!');
 });
 
-app.post('/queryWebsite', async(req, res) => {
+app.post('/queryWebsite', async (req, res) => {
 
     const websites = req.body;
     //fileter null and empty url
@@ -51,7 +51,9 @@ app.post('/queryWebsite', async(req, res) => {
 
 //queryWebsite route
 app.get('/queryWebsite', async (req, res) => {
-    const {website} = req.query;
+    const {
+        website
+    } = req.query;
     res.send(await mainFunction([website]));
 });
 
@@ -76,7 +78,7 @@ app.listen(port, () => {
 
 
 
-    
+
 // // url list 
 // const urlList = `
 //     http://www.amaya.biz/
@@ -111,8 +113,8 @@ async function mainFunction(urlList, moreData) {
     // open browser 
     const browser = await puppeteer.launch({
         headless: false,
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+        // executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
         // executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
     });
 
@@ -142,45 +144,65 @@ async function mainFunction(urlList, moreData) {
 
     // get data of all pages
     const titleList = await Promise.all(pagelist.map(async (item) => {
-        const page = item.page;
-        const title = await page.title();
+        try {
+            const page = item.page;
+            const title = await page.title();
 
-        const emails = await page.$$eval('a[href^="mailto:"]', links => links.map(link => link.href));
-        const cleanEmails = emails.map(item => item.replace('mailto:', '').split('?')[0]); // get clean email address from href ignore mailto: and ?subject=...
-        const uniqueEmails = [...new Set(cleanEmails)]; // remove duplicate emails 
+            const emails = await page.$$eval('a[href^="mailto:"]', links => links.map(link => link.href));
+            const cleanEmails = emails.map(item => item.replace('mailto:', '').split('?')[0]); // get clean email address from href ignore mailto: and ?subject=...
+            const uniqueEmails = [...new Set(cleanEmails)]; // remove duplicate emails 
 
-        const phoneNumbers = await page.$$eval('a[href^="tel:"]', links => links.map(link => link.href));
-        const cleanPhoneNumbers = phoneNumbers.map(item => item.replace('tel:', '')); // get clean phone number from href ignore tel:...
-        const uniquePhoneNumbers = [...new Set(cleanPhoneNumbers)]; // remove duplicate phone numbers
+            const phoneNumbers = await page.$$eval('a[href^="tel:"]', links => links.map(link => link.href));
+            const cleanPhoneNumbers = phoneNumbers.map(item => item.replace('tel:', '')); // get clean phone number from href ignore tel:...
+            const uniquePhoneNumbers = [...new Set(cleanPhoneNumbers)]; // remove duplicate phone numbers
 
-        const linkedin = await page.$eval('a[href*="linkedin"]', el => el.href).catch(() => '');
-        const facebook = await page.$eval('a[href*="facebook"]', el => el.href).catch(() => '');
-        const twitter = await page.$eval('a[href*="twitter"]', el => el.href).catch(() => '');
-        const instagram = await page.$eval('a[href*="instagram"]', el => el.href).catch(() => '');
-        const youtube = await page.$eval('a[href*="youtube"]', el => el.href).catch(() => '');
-        const pinterest = await page.$eval('a[href*="pinterest"]', el => el.href).catch(() => '');
-        const snapchat = await page.$eval('a[href*="snapchat"]', el => el.href).catch(() => '');
-        const whatsapp = await page.$eval('a[href*="whatsapp"]', el => el.href).catch(() => '');
-        const tiktok = await page.$eval('a[href*="tiktok"]', el => el.href).catch(() => '');
-        const telegram = await page.$eval('a[href*="telegram"]', el => el.href).catch(() => '');
-        
-        return {
-            "Phone_from_website": uniquePhoneNumbers,
-            "Emails": uniqueEmails,
-            "linkedin": linkedin,
-            "facebook": facebook,
-            "twitter": twitter,
-            "instagram": instagram,
-            "youtube": youtube,
-            "pinterest": pinterest,
-            "snapchat": snapchat,
-            "whatsapp": whatsapp,
-            "tiktok": tiktok,
-            "telegram": telegram,
-            "title": title,
-            "url": item.url,
-            // "index": item.index 
-        };
+            const linkedin = await page.$eval('a[href*="linkedin"]', el => el.href).catch(() => '');
+            const facebook = await page.$eval('a[href*="facebook"]', el => el.href).catch(() => '');
+            const twitter = await page.$eval('a[href*="twitter"]', el => el.href).catch(() => '');
+            const instagram = await page.$eval('a[href*="instagram"]', el => el.href).catch(() => '');
+            const youtube = await page.$eval('a[href*="youtube"]', el => el.href).catch(() => '');
+            const pinterest = await page.$eval('a[href*="pinterest"]', el => el.href).catch(() => '');
+            const snapchat = await page.$eval('a[href*="snapchat"]', el => el.href).catch(() => '');
+            const whatsapp = await page.$eval('a[href*="whatsapp"]', el => el.href).catch(() => '');
+            const tiktok = await page.$eval('a[href*="tiktok"]', el => el.href).catch(() => '');
+            const telegram = await page.$eval('a[href*="telegram"]', el => el.href).catch(() => '');
+
+            return {
+                "Phone_from_website": uniquePhoneNumbers,
+                "Emails": uniqueEmails,
+                "linkedin": linkedin,
+                "facebook": facebook,
+                "twitter": twitter,
+                "instagram": instagram,
+                "youtube": youtube,
+                "pinterest": pinterest,
+                "snapchat": snapchat,
+                "whatsapp": whatsapp,
+                "tiktok": tiktok,
+                "telegram": telegram,
+                "title": title,
+                "url": item.url,
+                // "index": item.index 
+            };
+        } catch (error) {
+            return {
+                "Phone_from_website": [],
+                "Emails": [],
+                "linkedin": "",
+                "facebook": "",
+                "twitter": "",
+                "instagram": "",
+                "youtube": "",
+                "pinterest": "",
+                "snapchat": "",
+                "whatsapp": "",
+                "tiktok": "",
+                "telegram": "",
+                "title": "",
+                "url": item.url,
+                // "index": item.index 
+            };
+        }
     }));
 
     // close the browser
@@ -189,7 +211,7 @@ async function mainFunction(urlList, moreData) {
 
     // marge the data with moreData by url
     if (moreData) {
-        const result = titleList.map(item => {  
+        const result = titleList.map(item => {
             const data = moreData.find(data => data.url === item.url);
             return {
                 ...item,
@@ -201,9 +223,3 @@ async function mainFunction(urlList, moreData) {
 
     return titleList;
 }
-
-
-
-
-
-
